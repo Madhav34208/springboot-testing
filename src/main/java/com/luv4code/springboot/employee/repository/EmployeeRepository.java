17a43bc77be8684d0f47bd20d85ef64922426ad3
Wentworth@34208 <Madhav.p.akv@gmail.com>
2024-10-18 13:31:05 +0530

    Three more methods tested in repository class, and one custom method also added in the repository.

--- src/main/java/com/luv4code/springboot/employee/repository/EmployeeRepository.java
+++ src/main/java/com/luv4code/springboot/employee/repository/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.luv4code.springboot.employee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    Employee findByEmail(String email);
 }
--- src/test/java/com/luv4code/springboot/employee/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/luv4code/springboot/employee/repository/EmployeeRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -26,5 +28,53 @@ class EmployeeRepositoryTest {
         //then
         assertThat(savedEmployee).isNotNull();
         assertThat(savedEmployee.getEmployeeId()).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("Find All Employees")
+    void testFindAllEmployees() {
+        //given
+        Employee e1 = Employee.builder().firstName("madhav").lastName("ponnana").email("madhav@gmail.com").build();
+        Employee e2 = Employee.builder().firstName("roja").lastName("tumma").email("roja@gmail.com").build();
+        repository.save(e1);
+        repository.save(e2);
+
+        //when
+        List<Employee> employeeList = repository.findAll();
+
+        //then
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("Find Employee By Id")
+    void testFindEmployeeById() {
+        //given
+        Employee e1 = Employee.builder().firstName("madhav").lastName("ponnana").email("madhav@gmail.com").build();
+        repository.save(e1);
+
+        //when
+        Employee savedEmployee = repository.findById(e1.getEmployeeId()).orElse(null);
+
+        //then
+        assertThat(savedEmployee).isNotNull();
+        assertThat(savedEmployee.getFirstName()).isEqualTo("madhav");
+        assertThat(savedEmployee.getEmployeeId()).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("Find Employee By Email")
+    void testFindEmployeeByEamil(){
+        //given
+        Employee e1 = Employee.builder().firstName("madhav").lastName("ponnana").email("madhav@gmail.com").build();
+        repository.save(e1);
+
+        //when
+        Employee employee = repository.findByEmail(e1.getEmail());
+
+        //then
+        assertThat(employee.getEmail()).isEqualTo("madhav@gmail.com");
+        assertThat(employee.getEmail()).isNotEqualTo("madhav@outlook.com");
     }
 }
